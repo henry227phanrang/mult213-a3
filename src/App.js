a2934508f059049a9de8fc0f340d49fb6590def1
Henry <duongtranngoc227@gmail.com>
2024-12-15 20:55:58 -0600

    create default files for reacct app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>Welcome to the French Learning App</h1>
+      <p>Let's start building!</p>
+    </div>
+  );
+}
+
+export default App;
